1c3edeac37112508e1208980867f41e4e138bad9
Kevin Treger <kevin.treger@zf.com>
2024-10-02 15:12:42 +0200

    display_raw_data as function

--- bikeshare.py
+++ bikeshare.py
@@ -218,6 +218,27 @@ def df_parter(df, size):
         yield df.iloc[i:i+size]
     return
 
+def display_raw_data(df):
+    #show raw data in groups of 5 rows if requested
+    while True:
+        try:
+            while input("Do you want to see five rows of raw data(Yes/No)?").title() == "Yes":
+                for part in df_parter(df,5):
+                    print(part)
+                    print('-'*40)
+
+                    while True:
+                        try:
+                            if input("Do you want to see the next five rows of raw data (Yes/No)?").title() != "Yes":
+                                return
+                            break
+                        except:
+                            print("Please respond with yes or no")
+        
+            break
+        except:
+            print("Please answer with yes or no")
+
 
 def main():
     while True:
@@ -232,33 +253,7 @@ def main():
             trip_duration_stats(df)
             user_stats(df)
             
-
-            #show raw data in groups of 5 rows if requested
-            while True:
-                try:
-                    question_raw_data = input("Do you want to see five rows of raw data (Yes/No)?").title()
-                    break
-                except:
-                    print("Please respond with yes or no")
-            
-            
-            #if the answer is 'Yes' then show the first 5 rows
-            while question_raw_data ==  "Yes":
-                for part in df_parter(df,5):
-                    print(part)
-                    print('-'*40)
-                 
-                    #ask if the next five rows should be displayed
-                    while True:
-                        try:
-                            question_raw_data = input("Do you want to see the next five rows of raw data(Yes/No)?").title()
-                            break
-                        except:
-                            print("Please respond with yes or no")
-                    
-                    #exit if answer is not 'Yes'
-                    if question_raw_data != "Yes":
-                        break
+            display_raw_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
